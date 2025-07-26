4dd89c0bf40a9fceb34acd23703e86d5be1357e1
JAntonioAR <jantonioagr@outlook.com>
2025-07-25 21:07:10 -0400

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit 1cf99f75729b793ffd451b54e832962efefe3200.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,5 +11,4 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 
- # Here is a new update by JAntonioAR
- # Here is another update by JAntonioAR
+ # Here is a new update by JAntonioAR 
